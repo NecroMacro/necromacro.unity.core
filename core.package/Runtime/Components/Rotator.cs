055efa06dabaf3595b69746e3efdf5c8eadd88ce
Andrei Milkov <128579301+andreimilkov@users.noreply.github.com>
2026-03-26 23:21:54 +0200

    Rotator

---
+++ core.package/Runtime/Components/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace NecroMacro.Core.Components
+{
+	public class Rotator : MonoBehaviour
+	{
+		public Vector3 rotationAxis = Vector3.up; // ось вращения
+		public float speed = 50f; // градусов в секунду
+
+		private void Update()
+		{
+			transform.rotation *= Quaternion.AngleAxis(speed * Time.deltaTime, rotationAxis.normalized);
+		}
+	}
+}
